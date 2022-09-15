58763254cbde15912d284d7508d10a3c49686a18
LoweKumar <lowekr00@gmail.com>
2022-09-15 12:58:00 +0530

    Lecture297,298-Modules and import, Standard python library

--- example.py
+++ example.py
@@ -1,3 +1,9 @@
-import random
-
-help(random.randint)
+# import random
+#
+# help(random.randint)
+import shelve
+help(shelve)
+# # print(dir())
+# # print(dir(__builtins__))
+# for m in dir(__builtins__):
+#     print(m)
